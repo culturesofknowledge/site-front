7af96f4466108003c5deeb881b1cc0997db3c6a6
Matthew Wilcoxson <matthew.wilcoxson@oerc.ox.ac.uk>
2019-01-16 14:27:10 +0000

    Tweak indexer.

--- pylons-emlo/emlo/workspace/indexing/src/index_generate_ids.py
+++ pylons-emlo/emlo/workspace/indexing/src/index_generate_ids.py
@@ -6,5 +6,4 @@ import indexer
 import redisconfig
 
 red_ids = redis.Redis(host=redisconfig.host, db=redisconfig.db_object_ids)
-
-indexer.GenerateIds( [],red_ids )
+indexer.GenerateIds(None, red_ids)
