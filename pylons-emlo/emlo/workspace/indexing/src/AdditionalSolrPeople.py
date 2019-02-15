de5686d6e7f305dd3ef7b6355b34e164e0489639
Matthew Wilcoxson <git@akademy.co.uk>
2019-02-15 13:54:47 +0000

    Using better solr library

--- pylons-emlo/emlo/workspace/indexing/src/AdditionalSolrPeople.py
+++ pylons-emlo/emlo/workspace/indexing/src/AdditionalSolrPeople.py
@@ -1,8 +1,9 @@
 # -*- coding: utf-8 -*-
 import sys
-import time
+import pprint
 
-import solr
+#import solr
+import pysolr
 
 # These two lines are hacks. They switch the default encoding to utf8 so that the command line will convert UTF8 + Ascii to UTF8
 reload(sys)
@@ -16,93 +17,142 @@ import fieldmap as f
 from helpers import escape_colons
 from helpers import get_records_from_solr, uuid_from_uri
 
-start = 0
-batch = 200
+def main() :
 
-solr_people = solr.SolrConnection( solrconfig.solr_urls['people'],  persistent=True )
+	start = 0
+	batch = 200
 
-people = solr_people.query( "*:*", fields="*", start=start, rows=batch, score=False)
-#people = solr_people.query( "uuid:83132068-65cf-4c3f-b6c9-9e7ea18abee0", fields="*", start=start, rows=batch, score=False)
-#people = solr_people.query( "uuid:b6b697b2-d342-407d-b168-1cd702bda2bc", fields="*", start=start, rows=batch, score=False)
-#people = solr_people.query( "uuid:6a9b17f5-e7ce-4da2-ab67-9a58cdaecfcf", fields="*", start=start, rows=batch, score=False)
-#people = solr_people.query( "uuid:c442cfce-636a-480d-a959-2f3d8f7498a0", fields="*", start=start, rows=batch, score=False)
+	solr = pysolr.Solr(solrconfig.solr_urls['people'])
 
-total = people.numFound
-count_down = total
-while start < total :
+	fields = ",".join([
+		'uuid',
+		'frbr_creatorOf-work',
+		'mail_recipientOf-work',
+		'dcterms_identifier-editi_'
+	])
 
-	people_updates = []
-	for result in people.results:
-
-		updated = {}
-		changed = False
-
-		print count_down, result['id']
-
-		if 'frbr_creatorOf-work' in result:
-			works_created = get_records_from_solr( result['frbr_creatorOf-work'] , [
-				'mail_recipient-person', # person sent to
-				'mail_origin-location', # place wrote in
-			] )
-
-			if works_created:
-				# print works_created
-
-				updated['works_created_locations'] = set()
-				updated['works_to_people'] = set()
-
-				for work in works_created.itervalues() :
-					if 'mail_origin-location' in work:
-						for location in work['mail_origin-location'] :
-							updated['works_created_locations'].add( uuid_from_uri( location ) )
-
-					if 'mail_recipient-person' in work:
-						for person in work['mail_recipient-person'] :
-							updated['works_to_people'].add( uuid_from_uri( person ) )
-
-		if 'mail_recipientOf-work' in result:
-			works_recipient = get_records_from_solr( result['mail_recipientOf-work'] , [
-				'frbr_creator-person', # person received from
-				'mail_destination-location' # place received from
-			] )
-
-			if works_recipient:
-				# print works_recipient
-
-				updated['works_received_locations'] = set()
-				updated['works_from_people'] = set()
-
-				for work in works_recipient.itervalues() :
-
-					if 'mail_destination-location' in work :
-						for location in work['mail_destination-location']:
-							updated['works_received_locations'].add( uuid_from_uri( location ) )
-
-					if 'frbr_creator-person' in work:
-						for person in work['frbr_creator-person']:
-							updated['works_from_people'].add( uuid_from_uri( person ) )
+	people = solr.search( "*:*", **{
+	#people = solr.search( "uuid:06dc4197-2486-424b-831b-d5f45f077f23", **{
+		'fields' : fields,
+		'start' : start,
+		'rows' : batch,
+		'score' : False
+	})
 
 
-		if updated:
+	# count_down = total = people.numFound
+	count_down = total = people.hits
+	while start < total :
 
-			# print updated
-			for key, value in result.iteritems():
-				if key not in ['id', 'name-strict', 'foaf_name-firstletter', '_version_' ] : # These keys are solr copy-fields and shouldn't be indexed directly.
-					updated[key] = value
+		people_updates = []
+		for result in people.docs:  # people.results:
 
-			# print updated
-			people_updates.append( updated )
+			updated = {}
+			changed = False
+
+			print count_down, result['id']
+
+			if 'frbr_creatorOf-work' in result:
+				works_created = get_records_from_solr( result['frbr_creatorOf-work'] , [
+					'mail_recipient-person',  # person sent to
+					'mail_origin-location',  # place wrote in
+				] )
+
+				if works_created:
+					# print works_created
+
+					works_created_locations = set()
+					works_to_people = set()
+
+					for work in works_created.itervalues() :
+						if 'mail_origin-location' in work:
+							for location in work['mail_origin-location'] :
+								works_created_locations.add( uuid_from_uri( location ) )
+
+						if 'mail_recipient-person' in work:
+							for person in work['mail_recipient-person'] :
+								works_to_people.add( uuid_from_uri( person ) )
+
+					if len( works_created_locations ):
+						updated[u'works_created_locations'] = works_created_locations
+
+					if len( works_created_locations ):
+						updated[u'works_to_people'] = works_to_people
+
+			if 'mail_recipientOf-work' in result:
+				works_recipient = get_records_from_solr( result['mail_recipientOf-work'] , [
+					'frbr_creator-person',  # person received from
+					'mail_destination-location'  # place received from
+				] )
+
+				if works_recipient:
+					# print works_recipient
+
+					works_received_locations = set()
+					works_from_people = set()
+
+					for work in works_recipient.itervalues() :
+
+						if 'mail_destination-location' in work :
+							for location in work['mail_destination-location']:
+								works_received_locations.add( uuid_from_uri( location ) )
+
+						if 'frbr_creator-person' in work:
+							for person in work['frbr_creator-person']:
+								works_from_people.add( uuid_from_uri( person ) )
+
+					if len( works_received_locations ) :
+						updated[u'works_received_locations'] = works_received_locations
+
+					if len( works_from_people ) :
+						updated[u'works_from_people'] = works_from_people
+
+			if updated:
+				updated[u"dcterms_identifier-uuid_"] = u"uuid_" + result['uuid']
+				updated[u'uuid_related'] = ['00000001-0000-4da2-0000-000000000001']
+				#							#'6a9b17f5-e7ce-4da2-ab67-9a58cdaecfcf']
+
+				# print updated
+				#for key, value in result.iteritems():
+				#	if key not in ['id', 'name-strict', 'foaf_name-firstletter', '_version_' ] : # These keys are solr copy-fields and shouldn't be indexed directly.
+				#		updated[key] = value
+
+				# add dummy so it works:
+				#updated[u'uuid_related'] = ['6a9b17f5-e7ce-4da2-ab67-9a58cdaecfcf']
+				# print updated
+				#
+				people_updates.append( updated )
+
+			count_down -= 1
+
+		#solr_people.add_many( people_updates, False )
+		for person_updates in people_updates :
+
+			try:
+				solr.add([person_updates], fieldUpdates={
+					'works_created_locations': 'set',
+					'works_to_people': 'set',
+					'works_received_locations': 'set',
+					'works_from_people': 'set'
+				})
+			except pysolr.SolrError as se:
+				# catch bug in solr, raise if something else
+				if "TransactionLog" not in se.message or "java.util.UUID" not in se.message :
+					raise
 
 
-		solr_people.add_many( people_updates, False )
+		print "...at " + str(start)
+		start += batch
+		people = solr.search( "*:*", **{
+			'fields' : fields,
+			'start' : start,
+			'rows' : batch,
+			'score' : False
+		})
 
-		count_down -= 1
+	solr.commit()
+	# solr_people.close()
 
-	print "...at " + str(start)
-	start += batch
-	people = solr_people.query( "*:*", fields="*", start=start, rows=batch, score=False)
+	print "done " + str(start) + "(from " + str(total) + ")"
 
-solr_people.commit()
-solr_people.close()
-
-print "done " + str(start) + "(from " + str(total) + ")"
+main()
