9fffa89c5a3039ff2993a8ecb4ccc1bf0a9dcfcc
Matthew Wilcoxson <git@akademy.co.uk>
2019-05-07 21:51:11 +0100

    Create solr files.

--- create_solr_files.sh
+++ create_solr_files.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/env bash
 
-# if the solr instances have already been flipped you may need to flip them back... see indexer.py
+# if the solr instances have already been flipped you may need to switch stage1 with stage2 so it is pointing to the right core
+stage1="_stage"
+stage2=""
 
 for name in all comments images institutions locations manifestations people resources works
 do
 	echo "#Written by CorePropertiesLocator
 #`date`
 name=${name}${stage1}" > solr-emlo/solr/home/${name}/core.properties
-done
 
-for name in all_stage comments_stage images_stage institutions_stage locations_stage manifestations_stage people_stage resources_stage works_stage
-do
 	echo "#Written by CorePropertiesLocator
 #`date`
-name=${name}${stage2}" > solr-emlo/solr/home/${name}/core.properties
+name=${name}${stage2}" > solr-emlo/solr/home/${name}_stage/core.properties
+
 done
